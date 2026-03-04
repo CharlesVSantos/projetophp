cb8667c2005a97770231aa1de4496ed6b511a606
charlesvsantos <charlesmetals1@gmail.com>
2026-03-03 21:42:02 -0300

    trabalhando com declaração de variaveis

---
+++ segundo.php
@@ -0,0 +1,15 @@
+<?php 
+   // comentário de linha
+
+   $num1=10;
+   $num2=10.5;
+   $nome="Etecia";
+   $sobrenome= "Agostina"; 
+   $status = true;
+   $flag = false;
+
+   $resp = $num1 + $num2;
+   $resp = $nome + $sobrenome;
+
+
+ ?>
